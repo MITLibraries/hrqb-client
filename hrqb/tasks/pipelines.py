40f98c3bb7a9e0484ea6e4cb12f24d0763930c42
Graham Hukill <ghukill@gmail.com>
2024-05-29 17:03:53 -0400

    Tasks for LibHREmployeeAppointments table
    Why these changes are being introduced:
    
    HRQBClient needs to support a unique pipeline that will load static
    data from HR into a Quickbase table.  This pipeline will be run
    quite rarely, and would be suitable to do so manually from the CLI
    locally; an initial load and potentially bulk updates from HR.
    After that, this table will be managed directly in QB.  The data
    in this table is utilized by other tasks and pipelines.
    
    How this addresses that need:
    * Creates new UpdateLibHRData pipeline and associated tasks
    
    Side effects of this change:
    * Ability to load and update LibHREmployeeAppointments table via
    HRQBClient
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/HRQB-15

--- hrqb/tasks/pipelines.py
+++ hrqb/tasks/pipelines.py
@@ -14,3 +14,36 @@ class FullUpdate(HRQBPipelineTask):
         from hrqb.tasks.employees import LoadEmployees
 
         yield LoadEmployees(pipeline=self.pipeline_name)
+
+
+class UpdateLibHRData(HRQBPipelineTask):
+    """Pipeline to load Library HR employee appointment data from static CSV file.
+
+    This pipeline loads the table 'LibHR Employee Appointments', which contains
+    information known only by Library HR, that we cannot get from the data warehouse,
+    including:
+        - position HC ID (Headcount ID)
+        - position supervisor
+        - position library department
+        - position cost object
+
+    This Quickbase table is used by the 'Employee Appointments' table to fill in gaps from
+    warehouse data alone.  This pipeline is useful for initial loading and bulk changes,
+    but this table is primarily managed directly in Quickbase by HR staff.
+
+    This pipeline requires a 'csv_filepath' parameter is defined when running, e.g.
+        pipenv run hrqb --verbose /
+        pipeline -p UpdateLibHRData /
+        --pipeline-parameters=csv_filepath=<PATH TO CSV> /
+        run
+    """
+
+    csv_filepath = luigi.Parameter()
+
+    def requires(self) -> Iterator[luigi.Task]:  # pragma: no cover
+        from hrqb.tasks.libhr_employee_appointments import LoadLibHREmployeeAppointments
+
+        yield LoadLibHREmployeeAppointments(
+            pipeline=self.pipeline_name,
+            csv_filepath=self.csv_filepath,
+        )
