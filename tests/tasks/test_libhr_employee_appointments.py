f8cbabb7d04224d274af07f5238fc92d6a88cc51
Graham Hukill <ghukill@gmail.com>
2024-09-17 16:11:43 -0400

    Upate LibHR transform and load
    Why these changes are being introduced:
    
    A new requirement was added that table 'LibHR Employee Appointments'
    could have multiple rows for the same employee, but different headcount
    IDs (HC ID).  This would represent previous positions they held.
    
    Related, this Quickbase table would need to support rows for
    old headcount IDs for employees, requiring a way to distinguish
    'active' from 'inactive' rows.
    
    How this addresses that need:
    * Added new 'Active' column to Quickbase table 'LibHR Employee Appointments'
    * Add new 'Key' field to same table that is used for merge updates
      * this field is MD5 of MIT ID + Headcount ID (HC ID)
    
    Side effects of this change:
    * None
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/HRQB-49

--- tests/tasks/test_libhr_employee_appointments.py
+++ tests/tasks/test_libhr_employee_appointments.py
@@ -1,6 +1,5 @@
 # ruff: noqa: PD901, PLR2004
 
-import numpy as np
 import pandas as pd
 
 
@@ -18,39 +17,23 @@ def test_transform_libhr_employee_appointments_merge_departments(
     task_transform_libhr_employee_appointments,
 ):
     new_df = task_transform_libhr_employee_appointments.get_dataframe()
-    assert new_df.equals(
-        pd.DataFrame(
-            [
-                {
-                    "Related Employee MIT ID": 123456789,
-                    "Related Supervisor MIT ID": 444444444,
-                    "Cost Object": 555555555,
-                    "HC ID": "L-001",
-                    "Position ID": 888888888,
-                    "Related Department ID": 35.0,
-                },
-                {
-                    "Related Employee MIT ID": 987654321,
-                    "Related Supervisor MIT ID": 444444444,
-                    "Cost Object": 555555555,
-                    "HC ID": "L-100",
-                    "Position ID": 999999999,
-                    "Related Department ID": 40.0,
-                },
-                {
-                    "Related Employee MIT ID": 987654321,
-                    "Related Supervisor MIT ID": 444444444,
-                    "Cost Object": 555555555,
-                    "HC ID": "L-100",
-                    "Position ID": 999999991,
-                    "Related Department ID": np.nan,
-                },
-            ]
-        )
-    )
+    assert new_df.iloc[0]["Related Department ID"] == 35.0
+    assert new_df.iloc[1]["Related Department ID"] == 40.0
+    assert pd.isna(new_df.iloc[2]["Related Department ID"])  # no match in merge, so NULL
 
 
 def test_load_libhr_employee_appointments_merge_field_set(
     task_load_libhr_employee_appointments,
 ):
-    assert task_load_libhr_employee_appointments.merge_field == "Position ID"
+    assert task_load_libhr_employee_appointments.merge_field == "Key"
+
+
+def test_transform_libhr_employee_appointments_merge_field_key_values(
+    task_transform_libhr_employee_appointments,
+):
+    new_df = task_transform_libhr_employee_appointments.get_dataframe()
+    assert list(new_df["Key"]) == [
+        "81cf06bfd65aa1f7019750c57a79be99",
+        "6e07102ee39ec1f22c63231d090bd4dd",
+        "744aefdd46c40523d60cf69490d81655",
+    ]
